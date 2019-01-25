4a191f47f9ea0b5e8f4be0190be21ec38669a14c
Eetu Rantala <rantala.eetu@gmail.com>
2019-01-25 23:31:58 +0200

    Pelaajan kääntyminen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    Transform ukkeli;
+    Vector2 oldlocalscale;
     public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ukkeli = transform.GetChild(0);
+        oldlocalscale = ukkeli.localScale;
     }
 
     // Update is called once per frame
@@ -17,5 +19,16 @@ public class PlayerController : MonoBehaviour
     {
         float direction = Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * direction * Time.deltaTime * speed);
+        
+
+        // TODO Käännä ukkeli
+
+        //ukkeli.localScale = localscale;
+        if(direction != 0) {
+            if(direction < 0)
+                ukkeli.localScale = new Vector2(-oldlocalscale.x, oldlocalscale.y);
+            else
+                ukkeli.localScale = oldlocalscale;
+        }
     }
 }
