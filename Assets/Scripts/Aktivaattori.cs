0a668e82a15eb04fa47a612d14eccccca9db8dd9
Eetu Rantala <rantala.eetu@gmail.com>
2019-01-25 22:12:17 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Aktivaattori.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aktivaattori : MonoBehaviour
+{
+    public Transform panel;
+    bool playerOnTrigger = false;
+    
+    void Update() {
+        if (playerOnTrigger && Input.GetKeyDown("space"))
+        {
+            if(panel.gameObject.activeInHierarchy == false) {
+                Debug.Log("auki");
+                panel.gameObject.SetActive(true);
+            }
+            else {
+                Debug.Log("kiinni");
+                panel.gameObject.SetActive(false);
+            }
+        }
+        else if (Input.GetKeyDown("space")) {
+            panel.gameObject.SetActive(false);
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+            playerOnTrigger = true;
+    }
+    
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.tag == "Player")
+            playerOnTrigger = false;
+            panel.gameObject.SetActive(false);
+    }
+}
