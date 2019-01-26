441af5f2adeda5022a43533ef5a0fe947e1cc58d
Eetu Rantala <rantala.eetu@gmail.com>
2019-01-26 12:12:20 +0200

    Puukkojamppa

---
+++ Assets/Scripts/Puukkojamppa.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puukkojamppa : MonoBehaviour
+{
+    public float speed = 3;
+    void Start() {
+        
+    }
+    void Update() {
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player") {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TriggerEnter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnter : MonoBehaviour
+{
+    public GameObject puukkojamppa;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        puukkojamppa.SetActive(true);
+    }
+}
