07a6fbea6d5b081b04be473e0b2250f92e8ac1ce
Eetu Rantala <rantala.eetu@gmail.com>
2019-01-26 18:24:27 +0200

    animaatiot vaihtuu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Transform ukkeli;
+    Animator animator;
     Vector2 oldlocalscale;
     public float speed = 10.0f;
     // Start is called before the first frame update
@@ -12,20 +13,27 @@ public class PlayerController : MonoBehaviour
     {
         ukkeli = transform.GetChild(0);
         oldlocalscale = ukkeli.localScale;
+        animator = ukkeli.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float direction = Input.GetAxis("Horizontal");
+
+        if(direction == 0) {
+            animator.Play("Idle");
+        }
+        else {
+            animator.Play("Walking");
+        }
+
         if(transform.position.x > -10 || direction > 0) {
             transform.Translate(Vector2.right * direction * Time.deltaTime * speed);
         }
+
         
-
-        // TODO Käännä ukkeli
-
-        //ukkeli.localScale = localscale;
+        
         if(direction != 0) {
             if(direction < 0)
                 ukkeli.localScale = new Vector2(-oldlocalscale.x, oldlocalscale.y);
--- Assets/Scripts/Puukkojamppa.cs
+++ Assets/Scripts/Puukkojamppa.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Puukkojamppa : MonoBehaviour
 {
+    public bool playerStabbed = false;
+    Animator animator;
     public float speed = 3;
     void Start() {
+        animator = transform.GetChild(0).GetComponent<Animator>();
         if(gameObject.activeSelf) {
             gameObject.SetActive(false);
         }
@@ -14,9 +17,15 @@ public class Puukkojamppa : MonoBehaviour
         transform.Translate(Vector2.right * Time.deltaTime * speed);
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Player") {
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.tag == "Player") {
             Destroy(other.gameObject);
+            playerStabbed = true;
         }
+    }
+
+    void Stoppaa() {
+        speed = 0;
+        animator.Play("Idle");
     }
 }
