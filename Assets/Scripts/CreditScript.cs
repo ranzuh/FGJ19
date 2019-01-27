de39d37ced1750a5dc9c5bad61ce3583f8db13ba
Munski <antti.muunoja@hotmail.fi>
2019-01-27 14:03:10 +0200

    End scene and main menu fixes

---
+++ Assets/Scripts/CreditScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScript : MonoBehaviour
+{
+
+    Animator animator;
+    public float speed = 3;
+
+
+    void Start()
+    {
+        animator = transform.GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
+    }
+
+
+    void Stoppaa()
+    {
+        speed = 0;
+        animator.Play("Idle");
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Bed")
+        {
+            Debug.Log("Stop");
+            Stoppaa();
+        }
+    }
+}
---
+++ Assets/Scripts/CreditsBlackScreenScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsBlackScreenScript : MonoBehaviour
+{
+
+    public CanvasGroup canvasGroup;
+    public bool playerOnTrigger = false;
+    public Transform creditPanel;
+
+
+
+    private void Update()
+    {
+        if (playerOnTrigger)
+        {
+            StartCoroutine(BlackCanvas());
+                if (canvasGroup.alpha == 1)
+                {
+                    creditPanel.gameObject.SetActive(true);
+                }
+        }
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("true");
+            playerOnTrigger = true;
+        }
+    }
+
+
+    public IEnumerator BlackCanvas()
+    {
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += Time.deltaTime / 10;
+            yield return null;
+        }
+        canvasGroup.interactable = false;
+        yield return null;
+    }
+
+
+    public void ButtonPressed()
+    {
+        playerOnTrigger = false;
+    }
+
+}
